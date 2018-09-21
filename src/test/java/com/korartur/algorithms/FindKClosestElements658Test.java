bf7efe2c2a66b8ec9c4f30a05a3711e3eba196a3
Artur Korop <rubkaarchi@gmail.com>
2018-09-21 18:31:25 +0200

    Add task about find k closest elements

---
+++ src/test/java/com/korartur/algorithms/FindKClosestElements658Test.java
@@ -0,0 +1,19 @@
+package com.korartur.algorithms;
+
+import com.koratur.algorithms.FindKClosestElements658;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindKClosestElements658Test {
+
+    @Test
+    public void test1(){
+        var f = new FindKClosestElements658();
+
+        Assert.assertArrayEquals(new int[]{1,2,3,4},
+                f.findClosestElements(new int[]{1,2,3,4,5},4,3)
+                        .stream().mapToInt(x->x).toArray());
+    }
+
+
+}
