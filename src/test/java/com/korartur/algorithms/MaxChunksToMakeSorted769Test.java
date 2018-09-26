14b2d5b81a535af3b04b2b1fce74725beb26f7bf
Artur Korop <korartur@amazon.lu>
2018-09-26 18:18:25 +0200

    Add 2 tasks

---
+++ src/test/java/com/korartur/algorithms/MaxChunksToMakeSorted769Test.java
@@ -0,0 +1,23 @@
+package com.korartur.algorithms;
+
+import com.koratur.algorithms.MaxChunksToMakeSorted769;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxChunksToMakeSorted769Test {
+
+    @Test
+    public void test1(){
+        var m = new MaxChunksToMakeSorted769();
+
+        Assert.assertEquals(1, m.maxChunksToSorted(new int[]{4,3,2,1,0}));
+    }
+
+    @Test
+    public void test2(){
+        var m = new MaxChunksToMakeSorted769();
+
+        Assert.assertEquals(4, m.maxChunksToSorted(new int[]{1,0,2,3,4}));
+    }
+
+}
